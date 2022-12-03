1310f15920a45555c13d5a3e1761ca5bad59aec1
Torben Hansen <derhansen@gmail.com>
2022-12-03 18:53:36 +0100

    [TASK] Set min php version to 7.2 in ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.29-11.5.99',
-            'php' => '7.4.0-8.1.99',
+            'php' => '7.2.0-8.1.99',
         ],
         'conflicts' => [],
         'suggests' => [],
