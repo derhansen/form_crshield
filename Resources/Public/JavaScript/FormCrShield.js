e6ee71fc5f39f47c5eedb804248153d79e605529
Marcus Förster <m.foerster@xerc.de>
2023-02-05 14:07:51 +0100

    [OPT] JavaScript

--- Resources/Public/JavaScript/FormCrShield.js
+++ Resources/Public/JavaScript/FormCrShield.js
@@ -1,26 +1,17 @@
 /**
  * Required JavaScript for challenge/response spam protection
  */
-document.addEventListener('DOMContentLoaded', function () {
-  let elements = document.querySelectorAll('[data-cr-challenge]');
-  if (typeof elements === 'undefined' || elements === null || elements.length === 0) {
-    return;
-  }
-
-  let elementsArray = [...elements];
-  elementsArray.forEach(element => {
-    let challengeString = atob(element.getAttribute('data-cr-challenge'));
-    let [exp,challenge] = challengeString.split('|');
-    let delay = element.dataset.crDelay;
-
-    // ROT13 the challenge - source: https://stackoverflow.com/a/617685/1744743
-    let response = challenge.replace(/[a-zA-Z]/g, function (c) {
-      return String.fromCharCode((c <= 'Z' ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
-    });
-
-    // Set calculated response after defined amount of seconds
-    setTimeout(() => {
-      element.value = btoa(exp + '|' + response)
-    }, delay * 1000)
-  });
+document.addEventListener('DOMContentLoaded', () =>
+{
+  [...document.querySelectorAll('[id$="-cr-field"]')].forEach(element =>
+  {
+    let [delay,expiration,challenge] = atob(element.value).split('|');
+    setTimeout(() =>
+    {
+      element.value = btoa(expiration + '|' + challenge.replace(/[a-z]/gi, char =>
+      {
+        return String.fromCharCode( char.charCodeAt(0) + ( char.toUpperCase() <= 'M' ? 13 : -13 ) )/* https://stackoverflow.com/a/14553231 */
+      }))
+    }, 1e3 * delay)
+  })
 });
