bd31da849eb2c09160aa46d98c655448b94bb8f2
Marcus Förster <m.foerster@xerc.de>
2023-09-15 17:38:12 +0200

    [FIX] browser remember challenge

--- Classes/Hooks/Form.php
+++ Classes/Hooks/Form.php
@@ -47,11 +47,12 @@ class Form
             // Set delay for initial form (no delay for re-submission of form)
             $extensionSettings = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get('form_crshield');
             $delay = $runtime->getFormSession() === null ? (int)($extensionSettings['crJavaScriptDelay'] ?? 0) : 0;
-            $challenge = $delay . '|' . $expirationTime . '|' . GeneralUtility::hmac($expirationTime, $this->getHmacSalt($runtime));
+            $challenge = $expirationTime . '|' . GeneralUtility::hmac($expirationTime, $this->getHmacSalt($runtime)) . '|' . $delay;
 
             $newElement = $pageObject->createElement(self::FIELD_ID, 'Hidden');
             $newElement->addValidator(new NotEmptyValidator());
             $newElement->setDefaultValue(base64_encode($challenge));
+            $newElement->setProperty('fluidAdditionalAttributes', ['autocomplete' => 'off']);
         }
 
         return $currentPage;
--- Resources/Public/JavaScript/FormCrShield.js
+++ Resources/Public/JavaScript/FormCrShield.js
@@ -5,13 +5,13 @@ document.addEventListener('DOMContentLoaded', () =>
 {
   [...document.querySelectorAll('[id$="-cr-field"]')].forEach(element =>
   {
-    let [delay,expiration,challenge] = atob(element.value).split('|');
+    let [expiration,challenge,delay] = atob(element.value).split('|');
     setTimeout(() =>
     {
       element.value = btoa(expiration + '|' + challenge.replace(/[a-z]/gi, char =>
       {
         return String.fromCharCode( char.charCodeAt(0) + ( char.toUpperCase() <= 'M' ? 13 : -13 ) )/* https://stackoverflow.com/a/14553231 */
       }))
-    }, 1e3 * delay)
+    }, 1e3 * (delay ?? 0))
   })
 });
