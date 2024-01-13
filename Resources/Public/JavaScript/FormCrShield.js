892ee9834116b6840158f7803e496352afe4ab85
Torben Hansen <derhansen@gmail.com>
2024-01-13 10:11:53 +0100

    [BUGFIX] Ensure CR field check works with server side validation
    Refs #21

--- Resources/Public/JavaScript/FormCrShield.js
+++ Resources/Public/JavaScript/FormCrShield.js
@@ -6,12 +6,18 @@ document.addEventListener('DOMContentLoaded', () =>
   [...document.querySelectorAll('[id$="-cr-field"]')].forEach(element =>
   {
     let [expiration,challenge,delay] = atob(element.value).split('|');
+
+    if (delay === undefined) {
+      // The CR field has already been submitted by the client, but server side validation may have failed
+      return;
+    }
+
     setTimeout(() =>
     {
       element.value = btoa(expiration + '|' + challenge.replace(/[a-z]/gi, char =>
       {
         return String.fromCharCode( char.charCodeAt(0) + ( char.toUpperCase() <= 'M' ? 13 : -13 ) )/* https://stackoverflow.com/a/14553231 */
       }))
-    }, 1e3 * (delay ?? 3))
+    }, 1e3 * delay)
   })
 });
