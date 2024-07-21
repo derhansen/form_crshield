4e55dba8002f464a3cb37a3f7bfb491df8dcd0de
Torben Hansen <derhansen@gmail.com>
2024-07-21 14:52:04 +0200

    [TASK] Ignore sys_http_report table in DB dump

--- Tests/Acceptance/Fixtures/dump-db.sh
+++ Tests/Acceptance/Fixtures/dump-db.sh
@@ -13,5 +13,6 @@ mysqldump --no-create-info \
           --ignore-table=typo3_formcrshield_acceptance_v13.cache_pagesection_tags \
           --ignore-table=typo3_formcrshield_acceptance_v13.cache_rootline \
           --ignore-table=typo3_formcrshield_acceptance_v13.cache_rootline_tags \
-          --ignore-table=typo3_formcrshield_acceptance_v13.tx_extensionmanager_domain_model_extension	 \
+          --ignore-table=typo3_formcrshield_acceptance_v13.tx_extensionmanager_domain_model_extension \
+          --ignore-table=typo3_formcrshield_acceptance_v13.sys_http_report \
           -u$DB_USERNAME -p$DB_PASSWORD_V8 -h127.0.0.1 --port 3406 typo3_formcrshield_acceptance_v13 >> typo3.sql
