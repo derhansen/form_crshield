0ce272540629d7d4700f95d648e8b657dd2ebf7c
Torben Hansen <derhansen@gmail.com>
2025-09-27 05:39:47 +0200

    [RELEASE] Release of version 3.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,10 +10,10 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
-    'version' => '2.0.1',
+    'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.2.0-13.4.99',
+            'typo3' => '13.4.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
